24c859c638e58460c8da60eca08e7744b790081f
RadsammyT <radsammyt@gmail.com>
2023-01-24 20:16:15 -0500

    GOOOOOOOOO

--- src/app.rs
+++ src/app.rs
@@ -13,6 +13,7 @@ pub struct PersonalWeb {
     images: Images,
 
     image_scale: f32,
+    icon_scale: f32,
 }
 
 impl Default for PersonalWeb {
@@ -21,6 +22,7 @@ impl Default for PersonalWeb {
             state_index: 1,
             images: Images::default(),
             image_scale: 1.0,
+            icon_scale: 20.0,
         }
     }
 }
@@ -89,10 +91,13 @@ impl eframe::App for PersonalWeb {
                     if ui.button("images/shitpost").clicked() {
                         self.state_index = 2;
                     }
+                    if ui.button("config").clicked() {
+                        self.state_index = 3;
+                    }
                     ui.add_space(15.0);
 
-                    image_hyperlink_button(ui, ctx, &self.images.git_logo, "My Github", "https://github.com/RadsammyT?tab=repositories",Vec2::new(20.0, 20.0));
-                    image_hyperlink_button(ui, ctx, &self.images.reddit, "My Reddit", "https://www.reddit.com/user/RadsammyT",Vec2::new(20.0, 20.0));
+                    image_hyperlink_button(ui, ctx, &self.images.git_logo, "My Github", "https://github.com/RadsammyT?tab=repositories",Vec2::new(self.icon_scale, self.icon_scale));
+                    image_hyperlink_button(ui, ctx, &self.images.reddit, "My Reddit", "https://www.reddit.com/user/RadsammyT",Vec2::new(self.icon_scale, self.icon_scale));
 
                     ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
                         egui::warn_if_debug_build(ui);
@@ -126,12 +131,23 @@ impl eframe::App for PersonalWeb {
                         self.images.middle_man.as_ref().unwrap().show(ui);
                     });
                     ui.label(RichText::new("what da heeeeeeeeeeell what da heeeeeeeeeeeeeeeeeeeell what da heeeeeeeeell").strong());
-                    ui.add(egui::Slider::new(&mut self.image_scale, 0.00..=1.30));
+                }
+
+                3 => {
+                    ui.horizontal(|ui| {
+                        ui.label("self.image_scale");
+                        ui.add(egui::Slider::new(&mut self.image_scale, 0.00..=1.30));
+                    });
+
+                    ui.horizontal(|ui| {
+                        ui.label("self.icon_scale");
+                        ui.add(egui::Slider::new(&mut self.icon_scale, 10.00..=30.00));
+                    });
 
                 }
 
                 _ => {
-                    ui.label(RichText::new(format!("Invalid state {} \nIf you got here after refreshing, do CTRL + F5 to force reload. \nOtherwise clear out your cookies/cache for this website. ", self.state_index)).color(Color32::RED));
+                    ui.label(RichText::new(format!("Invalid state {} \nIf you got here after refreshing, do CTRL + F5 to force reload. \nAs an alternative, you can clear out your cookies/cache for this website. ", self.state_index)).color(Color32::RED));
                 }
             }
 
